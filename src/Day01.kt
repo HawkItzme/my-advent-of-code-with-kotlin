70978846334a5fc5a3b422f1cb71852f91205c90
Hawk Itzme <smquasim016@gmail.com>
2024-12-10 17:22:55 +0530

    Day 1, Part 2 sol added

--- src/Day01.kt
+++ src/Day01.kt
@@ -3,14 +3,22 @@ import kotlin.math.abs
 fun main(){
     val lines = readInput("Day01")
     val (left, right) = lines.map { line ->
-        val first = line.substringBefore(" ").toInt()
-        val second = line.substringAfterLast(" ").toInt()
+        val first = line.substringBefore(" ").toLong()
+        val second = line.substringAfterLast(" ").toLong()
         first to second
     }.unzip()
 
+    //PART 1 SOL
     val result =  left.sorted().zip(right.sorted()).map { (first, second) ->
         abs(first - second)
     }.sum()
 
     println(result)
+
+    //PART 2 SOL
+    val freq : Map<Long, Int> = right.groupingBy { it }.eachCount()
+    left.fold(0L){ acc, num ->
+        acc + num * freq.getOrDefault(num, 0)
+    }.also (:: println )
+
 }
