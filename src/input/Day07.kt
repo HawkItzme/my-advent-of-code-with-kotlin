4930175fcc9cac6562a9d75fbcc824500575ec58
Hawk Itzme <smquasim016@gmail.com>
2024-12-23 14:24:55 +0530

    Day 7, Part 2 sol added

--- src/input/Day07.kt
+++ src/input/Day07.kt
@@ -12,12 +12,19 @@ fun main(){
     }
     val partA = measureTimedValue { part1(calibs) }
     println(partA)
-   // part1(lines)
-  //  part2(lines)
+
+    val partB = measureTimedValue { part2(calibs) }
+    println(partB)
 }
 
 private fun part1(calibs: List<Calibration>): Long = calibs.filter { calibration ->
     calibration.isValid()
+}.sumOf { calibration ->
+    calibration.result
+}
+
+private fun part2(calibs : List<Calibration>) : Long = calibs.filter { calibration ->
+    calibration.isValid2()
 }.sumOf { calibration ->
     calibration.result
 }
@@ -44,4 +51,72 @@ data class Calibration(val result : Long, val operands: List<Long>){
         }
         return false
     }
+
+
+    fun isValid2(): Boolean{
+        // Here, we generate Binary number that indicates
+        // 1: times
+        // 0: Plus
+        // 2: Concat, then we iterate
+        var operandSlots = operands.size - 1
+        val possibleCombos = 3.0.pow(operandSlots.toDouble()).toInt()
+        for (operatorCombinations in 0..<possibleCombos){
+            var calcRes = operands[0]
+            for (index in 0..<operands.lastIndex){
+                val b = operands[index + 1]
+                val opstr = operatorCombinations.toString(3)
+                val op = opstr.getOrNull(opstr.lastIndex - index) ?: '0'
+                when(op){
+                    '0' -> calcRes = calcRes + b
+                    '1' -> calcRes = calcRes * b
+                    '2' -> calcRes = (calcRes.toString() + b.toString()).toLong()
+                }
+            }
+            if (calcRes == result) return true
+        }
+        return false
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
