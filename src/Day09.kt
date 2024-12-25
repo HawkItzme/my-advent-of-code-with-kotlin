467547235da01353623f0c5d3fcadf550745a4e7
Hawk Itzme <smquasim016@gmail.com>
2024-12-25 13:34:59 +0530

    Day 9, Part 2 sol added

--- src/Day09.kt
+++ src/Day09.kt
@@ -10,6 +10,8 @@ fun main() {
     }
     val a = part1(list)
     println(a)
+    val b = part2(list)
+    println(b)
 }
 
 data class Segment(val len: Int, val id: Int){
@@ -39,4 +41,61 @@ private fun part1(list: List<Segment>) : Long{
         checkSum += idx * block
     }
     return checkSum
+}
+
+class BlockStorage(list: List<Segment>){
+    val storage = list.toMutableList()
+
+    fun firstGapOfMinimumSize(n: Int, beforeIndex: Int): Int?{
+        val index = storage.indexOfFirst { segment -> segment.id == -1 && segment.len >= n }
+        if (index >= beforeIndex) return null
+        if (index == -1) return null
+        return index
+    }
+
+    fun moveSegmentIntoGap(segmentIdx: Int, gapIdx : Int){
+        val gap = storage[gapIdx]
+        val segment  = storage[segmentIdx]
+        val newGapLength = gap.len - segment.len
+        val newGapSegment = Segment(newGapLength, -1)
+        storage[segmentIdx] = Segment(segment.len, -1)
+        storage[gapIdx] = segment
+        if (newGapSegment.len > 0){
+            storage.add(gapIdx + 1, newGapSegment)
+        }
+    }
+
+    fun materialize() : List<Int>{
+        return storage.flatMap { segment ->
+            List(segment.len) {
+                segment.id
+            }
+        }
+    }
+
+    fun checkSum() : Long{
+        return materialize()
+            .withIndex()
+            .filter { (_, block) -> block != -1}
+            .sumOf { (idx, block) -> idx.toLong() * block }
+    }
+
+}
+
+private fun attemptMove(blockStorage : BlockStorage): Boolean{
+    for ((idx, segment) in blockStorage.storage.withIndex().reversed()){
+        if (segment.isEmpty) continue
+        val target = blockStorage.firstGapOfMinimumSize(n = segment.len, beforeIndex = idx) ?: continue
+        blockStorage.moveSegmentIntoGap(idx, target)
+        return true
+    }
+    return false
+}
+
+private fun part2(list : List<Segment>) : Long{
+    val blockStorage = BlockStorage(list)
+    do {
+        val didMoveSuccessfully = attemptMove(blockStorage)
+    }while (didMoveSuccessfully)
+    return blockStorage.checkSum()
 }
