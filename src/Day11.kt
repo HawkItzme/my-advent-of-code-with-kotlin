190a6f99dec26905fca374926ae44c4c661dff67
Hawk Itzme <smquasim016@gmail.com>
2024-12-28 20:41:42 +0530

    Day 11, Part 2 sol added

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,3 +1,4 @@
+import org.w3c.dom.css.Counter
 import kotlin.time.measureTimedValue
 
 fun main() {
@@ -5,6 +6,8 @@ fun main() {
     val line = input.first().split(" ").map { string -> string.toLong() }
     val a = measureTimedValue { part1(line) }
     println("Part 1: $a")
+    val b = measureTimedValue { part2(line) }
+    println("Part 2: $b")
 }
 
 private fun part1(line: List<Long>): Int{
@@ -14,6 +17,55 @@ private fun part1(line: List<Long>): Int{
         modifiedLine = blink(modifiedLine)
     }
     return modifiedLine.size
+}
+
+private fun part2(line: List<Long>): Long{
+    var stoneCounts = line.groupingBy { num -> num }.eachCount()
+    var stoneList = NonMaterializedList()
+    for ((k,v) in stoneCounts){
+        stoneList.add(k.toLong(), v.toLong())
+    }
+    repeat(75){
+        stoneList = blinkGroup(stoneList)
+    }
+    return stoneList.coll.values.sumOf { integer -> integer.toLong() }
+}
+
+fun blinkGroup(stoneList : NonMaterializedList) : NonMaterializedList{
+    return buildNonMaterializedList {
+        for ((number, count) in stoneList.coll){
+            when{
+                number == 0L -> {
+                    add(1L, count)
+                }
+
+                number.toString().length % 2 == 0 -> {
+                    val len = number.toString().length
+                    val top = number.toString().take(len / 2).toLong()
+                    val bot = number.toString().takeLast(len / 2).toLong()
+                    add(top, count)
+                    add(bot, count)
+                }
+
+                else -> {
+                    add(number * 2024L, count)
+                }
+            }
+        }
+    }
+}
+
+data class NonMaterializedList(val coll: MutableMap<Long, Long> = mutableMapOf()){
+    fun add(b: Long, counter: Long){
+        val curr = coll[b] ?: 0L
+        coll[b] = curr + counter
+    }
+}
+
+fun buildNonMaterializedList(build: NonMaterializedList.() -> Unit) : NonMaterializedList{
+    val list = NonMaterializedList()
+    list.build()
+    return list
 }
 
 fun blink(stones: List<Long>): List<Long>{
